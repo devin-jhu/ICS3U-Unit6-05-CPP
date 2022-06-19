a249c1ca254be3d78101c64cb06b262e0c62a550
Devin Jhu <devin.jhu@mths.ca>
2022-06-19 02:56:16 +0000

    code

--- numbers.cpp
+++ numbers.cpp
@@ -1,38 +1,70 @@
 // Copyright (C) 2022 Devin Jhu All rights reserved
 //
 // Created by Devin Jhu
-// Created on May 2022
-// The loop adder
+// Created on June 2022
+// The average
 
 #include <iostream>
-#include <random>
+#include <list>
+#include <cmath>
+#include <string>
 
-int main() {
-    // this function finds the average of 10 random numbers
 
-    int numberArray[10];
-    int randomNumber;
+float FindAverage(std::list<float> gradeList) {
+    // this function calculates the average
+
+    float total = 0;
     float average;
+    std::list<int> listSize;
 
-    // random seed
-    std::random_device rseed;
-    std::mt19937 rgen(rseed());  // mersenne twister
-    std::uniform_int_distribution<int> idist(0, 100);  // random from 0 - 100
-
-    std::cout << "The random number average finder" << std::endl;
-
-    // process and output
-    for (int counter = 0; counter < 10; counter++) {
-        randomNumber = idist(rgen);
-        numberArray[counter] = randomNumber;
-        std::cout << "Random number: " << randomNumber << std::endl;
+    for (int grade : gradeList) {
+        total += grade;
     }
 
-    average = std::accumulate(std::begin(numberArray), std::end(numberArray),
-        0, std::plus<int>());
-    average /= 10;
+    average = total / gradeList.size();
 
-    std::cout << "\nThe average is " << average << "." << std::endl;
+    return average;
+}
+
+
+int main() {
+    // this function gets input then shows the average
+    std::list<float> gradeList;
+    std::string markString;
+    int mark;
+    int outputFix = 0;  // meant to prevent unnecessary output
+    float average;
+
+    std::cout <<
+    "This program calculates your average mark. Enter -1 to stop.\n"
+    << std::endl;
+
+    // input
+    while (true) {
+        std::cout << "Enter a mark (%): ";
+        std::cin >> markString;
+        try {
+            mark = std::stoi(markString);
+            if (mark == -1) {
+                break;
+            } else if (mark < 0) {
+                std::cout << "not a number" << std::endl;
+                outputFix += 1;
+                break;
+            }
+            gradeList.push_back(mark);
+        } catch (std::invalid_argument) {
+            std::cout << "not a number" << std::endl;
+            outputFix += 1;
+            break;
+        }
+    }
+
+    // output
+    if (outputFix == 0) {  // only runs if fix has been left at 0
+         average = FindAverage(gradeList);
+         std::cout << "\nThe average is " << average << "%." << std::endl;
+    }
 
     std::cout << "\nDone." << std::endl;
 }
